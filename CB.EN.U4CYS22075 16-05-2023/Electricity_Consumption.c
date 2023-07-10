2ee30db864213e6d5016e043a9f67a65dfffec59
Vishal S <116857521+Vishal-S-14@users.noreply.github.com>
2023-07-10 15:28:14 +0530

    Update Electricity_Consumption.c

--- CB.EN.U4CYS22075 16-05-2023/Electricity_Consumption.c
+++ CB.EN.U4CYS22075 16-05-2023/Electricity_Consumption.c
@@ -19,13 +19,13 @@ int main() {
         printf("Units consumed: ");
         scanf("%d", &units);
 
-        if (units <= 100) {
+        if (units > 0 && units <= 100) {
             bill = 0;
-        } else if (units <= 400) {
+        } else if (units > 100 && units <= 400) {
             bill = (units - 100) * 2.25;
-        } else if (units <= 500) {
+        } else if (units > 400 && units <= 500) {
             bill = (300 * 2.25) + (units - 400) * 4.50;
-        } else if (units <= 600) {
+        } else if (units > 500 && units <= 600) {
             bill = (300 * 2.25) + (100 * 4.50) + (units - 500) * 6.00;
         } else {
             bill = (300 * 2.25) + (100 * 4.50) + (100 * 6.00) + (units - 600) * 8.00;
@@ -35,11 +35,13 @@ int main() {
         printf("Name: %s\n", name);
         printf("EB Number: %s\n", ebNumber);
         printf("Units Consumed: %d\n", units);
-        
+
         if (units > 100) {
             printf("Bill Amount: %.2f\n", bill);
-        } else {
+        } else if (units > 0 && units <= 100) {
             printf("Free\n");
+        } else {
+            printf("Invalid Units\n");
         }
     }
     return 0;
